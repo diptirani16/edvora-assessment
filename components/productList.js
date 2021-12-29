c8c6bfd226d023a2ac8ef66f20498f4dd9df9173
Dipti Rani <gdipti642@gmail.com>
2021-12-29 17:29:57 +0530

    add styling to products lists

--- components/productList.js
+++ components/productList.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Typography, Divider, Paper, Card, Box, CardContent, CardMedia } from "@mui/material";
-import { productGrouping } from "../utils/productGrouping";
 
 export default function ProductionList(props) {
-    
+
     return (
         <>
             <Typography sx={{ fontSize: 34, fontWeight: 800 }} gutterBottom>
@@ -20,40 +19,44 @@ export default function ProductionList(props) {
                     <Divider />
                     <Paper elevation={0} sx={{ p: 2, borderRadius: 2, mt: 2, display: 'flex', flexWrap: 'nowrap', overflow: 'scroll' }}>
                         {i.map(j => (
-                            <Card sx={{ display: 'flex', p: 1, m:1, overflow: 'unset' }}>
-                                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                    <CardMedia
-                                        component="img"
-                                        sx={{ width: 70, height: 70, borderRadius: 2 }}
-                                        image={j.image}
-                                        alt="Live from space album cover"
-                                    />
-                                    <Typography color="text.secondary" sx={{ fontSize: 12 }}>
-                                        {j.address.state}
-                                    </Typography>
+                            <Card sx={{ p: 1, m: 1, overflow: 'unset' }}>
+                                <Box sx={{ display: 'flex' }}>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column', width: 120 }}>
+                                        <CardMedia
+                                            component="img"
+                                            sx={{ width: 70, height: 70, borderRadius: 2 }}
+                                            image={j.image}
+                                            alt="Live from space album cover"
+                                        />
+
+                                    </Box>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                        <CardContent sx={{ width: 100, display: 'flex', flexDirection: 'column', alignItems: 'space-around', p: 0 }}>
+                                            <Typography sx={{ fontSize: 15 }}>
+                                                {j.product_name}
+                                            </Typography>
+                                            <Typography color="text.secondary" sx={{ fontSize: 12 }}>
+                                                {j.brand_name}
+                                            </Typography>
+                                            <Typography sx={{ fontSize: 12 }}>
+                                                $ {j.price}
+                                            </Typography>
+
+                                        </CardContent>
+                                    </Box>
+                                    
                                 </Box>
-                                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                                    <CardContent sx={{ flex: '1 0 auto' }}>
-                                        <Typography sx={{ fontSize: 15 }}>
-                                            {j.product_name}
-                                        </Typography>
-                                        <Typography color="text.secondary" sx={{ fontSize: 12 }}>
-                                            {j.brand_name}
-                                        </Typography>
-                                        <Typography sx={{ fontSize: 12 }}>
-                                            $ {j.price}
+                                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                        <Typography color="text.secondary" sx={{ fontSize: 12, width: 100 }}>
+                                            {j.address.state}
                                         </Typography>
                                         <Typography color="text.secondary" sx={{ fontSize: 12 }}>
                                             Date: {j.date.slice(0, 10)}
                                         </Typography>
-                                    </CardContent>
-                                    {/* <Box sx={{ display: 'flex', pl: 1, pb: 1 }}> */}
-                                    <Typography color="text.secondary" sx={{ fontSize: 12 }}>
-                                        {j.discription}
-                                    </Typography>
-
-                                    {/* </Box> */}
-                                </Box>
+                                    </Box>
+                                <Typography color="text.secondary" sx={{ fontSize: 12 }}>
+                                    {j.discription}
+                                </Typography>
                             </Card>
                         ))}
 
